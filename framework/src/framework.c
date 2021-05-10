49f6f84b58a55ac4bf7c0583ea939ea9a8c87611
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-05-10 21:57:04 +0900

    work fine

--- framework/src/framework.c
+++ framework/src/framework.c
@@ -14,6 +14,7 @@ typedef struct		s_unit_test
 
 int test_atoi_1(void)
 {
+	printf("test_here!\n");
     if (0 == atoi("aaa_0"))
         return(0);
     return(1);
@@ -22,14 +23,44 @@ int test_atoi_1(void)
 void load_test(t_unit_test **testlist, char *title, int (* test_func)());
 int launch_tests(t_unit_test **testlist);
 
+size_t	ft_strlen(const char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (*(s + i) != 0)
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	int		strlen;
+	char	*return_p;
+	char	*dest;
+
+	strlen = ft_strlen(s1);
+	return_p = malloc(sizeof(char) * strlen + 1);
+	if (!return_p)
+		return (return_p);
+	dest = return_p;
+	while (*s1)
+		*dest++ = *s1++;
+	*dest = 0;
+	return (return_p);
+}
+
 void load_test(t_unit_test **testlist, char *title, int (* test_func)())
 {
     t_unit_test *new;
 
     new = malloc(sizeof(t_unit_test));
+	new->next = NULL;
+	new->title = ft_strdup(title);
+	new->func = test_func;
     if (!new)
         exit(0); //abort for malloc error;
-    if (testlist == NULL)
+    if (*testlist == NULL)
         *testlist = new;
     else
     {
@@ -44,6 +75,7 @@ int launch_tests(t_unit_test **testlist) //freamwork(/*複数のテスト(の配
     pid_t   pid;
     pid_t   wait_pid;
     int status;
+	t_unit_test *tmp;
 
     while(*testlist)
     {
@@ -53,6 +85,10 @@ int launch_tests(t_unit_test **testlist) //freamwork(/*複数のテスト(の配
         if(pid == 0)
             exit((*testlist)->func());
         wait_pid = wait(&status);
+		tmp = *testlist;
+		*testlist = (*testlist)->next;
+		free(tmp->title);
+		free(tmp);
     }
     if (WIFEXITED(status))
         return ((char)WEXITSTATUS(status));
