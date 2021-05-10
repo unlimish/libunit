687eead94cdf20a898dc84a712b4ada86312e536
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-05-10 20:49:44 +0900

    とりあえず、compile通ったので push しとく

--- framework/src/framework.c
+++ framework/src/framework.c
@@ -5,35 +5,71 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
-int test()
+typedef struct		s_unit_test
 {
-    char *s = "aiueo";
-    return (0);
+    int					(* func)();
+    char				*title;
+    struct s_unit_test	*next;
+}					t_unit_test;
+
+int test_atoi_1(void)
+{
+    if (0 == atoi("aaa_0"))
+        return(0);
+    return(1);
 }
 
-int freamwork()
+void load_test(t_unit_test **testlist, char *title, int (* test_func)());
+int launch_tests(t_unit_test **testlist);
+
+void load_test(t_unit_test **testlist, char *title, int (* test_func)())
+{
+    t_unit_test *new;
+
+    new = malloc(sizeof(t_unit_test));
+    if (!new)
+        exit(0); //abort for malloc error;
+    if (testlist == NULL)
+        *testlist = new;
+    else
+    {
+        while ((*testlist)->next)
+            *testlist = (*testlist)->next;
+        (*testlist)->next = new;
+    }
+}
+
+int launch_tests(t_unit_test **testlist) //freamwork(/*複数のテスト(の配列なり、リストなり)が引数*/)
 {
     pid_t   pid;
+    pid_t   wait_pid;
     int status;
 
-    pid = fork();
-    if(pid < 0)
-        exit(0);
-    if(pid == 0)
-        exit(test());
-    /*
-    wait(&status);
+    while(*testlist)
+    {
+        pid = fork(); 
+        if(pid < 0)
+            exit(0);
+        if(pid == 0)
+            exit((*testlist)->func());
+        wait_pid = wait(&status);
+    }
     if (WIFEXITED(status))
         return ((char)WEXITSTATUS(status));
     if (WIFSIGNALED(status))
         return (WTERMSIG(status));
-    */
     return (0);
 }
 
 int main()
 {
-    printf("return from freamwork[%d]\n", freamwork());
+    t_unit_test *testlist;
+
+    testlist = NULL;
+    load_test(&testlist, "Test 1", test_atoi_1);
+    launch_tests(&testlist);
+    
+//    printf("return from freamwork[%d]\n", freamwork());
 
     //printf("%d\n", ret);
 }
