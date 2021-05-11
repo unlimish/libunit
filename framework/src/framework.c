97404f78de3521ca291af7d3d785fb026976c3ea
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-05-11 21:26:26 +0900

    segv

--- framework/src/framework.c
+++ framework/src/framework.c
@@ -22,6 +22,20 @@ int test_atoi_1(void)
 		return(1);
 }
 
+int test_err(void)
+{
+	return(1);
+}
+
+int test_segv(void)
+{
+	char *a;
+
+	a = NULL;
+	*a = 'a';
+	return(0);
+}
+
 void load_test(t_unit_test **testlist, char *title, int (* test_func)());
 int launch_tests(t_unit_test **testlist);
 
@@ -55,7 +69,10 @@ char	*ft_strdup(const char *s1)
 void load_test(t_unit_test **testlist, char *title, int (* test_func)())
 {
     t_unit_test *new;
+	t_unit_test *iter;
 
+	printf("title : [%s]\n", title);
+	iter = *testlist;
     new = malloc(sizeof(t_unit_test));
 	new->next = NULL;
 	new->title = ft_strdup(title);
@@ -63,12 +80,16 @@ void load_test(t_unit_test **testlist, char *title, int (* test_func)())
     if (!new)
         exit(0); //abort for malloc error;
     if (*testlist == NULL)
+	{
+		printf("koko\n");
         *testlist = new;
+	}
     else
     {
-        while ((*testlist)->next)
-            *testlist = (*testlist)->next;
-        (*testlist)->next = new;
+		printf("kokoB\n");
+        while (iter->next)
+            iter = iter->next;
+        iter->next = new;
     }
 }
 
@@ -77,16 +98,23 @@ int launch_tests(t_unit_test **testlist) //freamwork(/*複数のテスト(の配
     pid_t   pid;
     pid_t   wait_pid;
     int status;
+	int ret;
 	t_unit_test *tmp;
 
     while(*testlist)
     {
+		printf("title:[%s]\n", (*testlist)->title);
         pid = fork();
         if(pid < 0)
             exit(0);
         if(pid == 0)
             exit((*testlist)->func());
-        wait_pid = wait(&status);
+		wait_pid = wait(&status);
+		if (WIFEXITED(status))
+			ret = WEXITSTATUS(status);
+		if (WIFSIGNALED(status))
+			ret = WTERMSIG(status);
+		printf("ret: [%d]\n", ret);
 		tmp = *testlist;
 		*testlist = (*testlist)->next;
 		free(tmp->title);
@@ -106,5 +134,7 @@ int main()
     testlist = NULL;
     load_test(&testlist, "Test 0", test_atoi_0);
     load_test(&testlist, "Test 1", test_atoi_1);
+    load_test(&testlist, "Test error", test_err);
+    load_test(&testlist, "Test segv", test_segv);
     launch_tests(&testlist);
 }
