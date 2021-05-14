2b0b4ca35358dd9bc4c4c4e53c9e5e9d92c90207
unlimish <mail@unlimish.xyz>
2021-05-14 14:40:54 +0900

    Update (main.c): Title Art

--- tests/main.c
+++ tests/main.c
@@ -1,19 +1,15 @@
 #include "inc/tests.h"
-#include "inc/utils.h"
 #include "../framework/inc/libunit.h"
 
 int	main(int argc, char **argv)
 {
-	int	list_len;
-	int pass_len;
-	t_unit_test	*testlist;
+	char *func;
 
-	puts("  ^~^  ,");
+	puts("\n\n  ^~^  ,");
 	puts(" ('Y') )");
 	puts(" /   \\/  \x1b[36m\x1b[1m42 Unit Tests\x1b[0m\x1b[39m  __QQ");
 	puts("(\\|||/)                (_)_\">>");
-	puts("                      /");
-	if ( argc == 1 || 0 == strncmp(argv[1],"strlen", strlen("strlen")))
+	puts("                      /\n");
 		strlen_launcher();
 	toupper_launcher();
 
