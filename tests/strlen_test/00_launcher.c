a78368155ce72439de01e529b6cff8be9dce29f2
unlimish <mail@unlimish.xyz>
2021-05-14 14:42:01 +0900

    Add (strlen_launcher): func title color

--- tests/strlen_test/00_launcher.c
+++ tests/strlen_test/00_launcher.c
@@ -6,9 +6,10 @@ int	strlen_launcher(void)
 	t_unit_test	*testlist;
 
 	testlist = NULL;
-	puts("STRLEN:");
+	printf("\x1b[43;30m STRLEN \x1b[0m\n");
 	load_test(&testlist, "Basic test", &strlen_basic_test);
 	load_test(&testlist, "NULL test", &strlen_null_test);
 	load_test(&testlist, "Basic char variable test", &strlen_basic_var_test);
+	color(RESET);
 	return(launch_tests(&testlist));
 }
