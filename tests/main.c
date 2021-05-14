e1d5b919eddc606a1c8aa423f448e2ca48461a64
unlimish <mail@unlimish.xyz>
2021-05-14 14:41:31 +0900

    Update (main.c): spacify func runner

--- tests/main.c
+++ tests/main.c
@@ -10,7 +10,11 @@ int	main(int argc, char **argv)
 	puts(" /   \\/  \x1b[36m\x1b[1m42 Unit Tests\x1b[0m\x1b[39m  __QQ");
 	puts("(\\|||/)                (_)_\">>");
 	puts("                      /\n");
+	func = "strlen";
+	if ( argc == 1 || 0 == strncmp(argv[1],func, strlen(func)))
 		strlen_launcher();
+	func = "toupper";
+	if ( argc == 1 || 0 == strncmp(argv[1],func, strlen(func)))
 	toupper_launcher();
 
 
