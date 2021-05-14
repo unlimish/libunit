5f93737a03cc1857a133473ca1226dad34e763d7
unlimish <mail@unlimish.xyz>
2021-05-14 15:34:51 +0900

    Update (main): title with under line

--- tests/main.c
+++ tests/main.c
@@ -7,7 +7,7 @@ int	main(int argc, char **argv)
 
 	puts("\n\n  ^~^  ,");
 	puts(" ('Y') )");
-	puts(" /   \\/  \x1b[36m\x1b[1m42 Unit Tests\x1b[0m\x1b[39m  __QQ");
+	puts(" /   \\/  \x1b[36;1;4m42 Unit Tests\x1b[0m\x1b[39m  __QQ");
 	puts("(\\|||/)                (_)_\">>");
 	puts("                      /\n");
 	func = "strlen";
